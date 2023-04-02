42ee8219de825605a639d85ce885c17abe898514
TriumphChinnah <109879773+TriumphChinnah@users.noreply.github.com>
2023-04-02 03:04:57 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+const change = document.getElementById("image1");
+
+change.addEventListener("click", (event) => {
+    event.target.style.background = "#00203FFF";
+    event.target.style.color = "white";
+});
+
+const change2 = document.getElementById("image2");
+
+change2.addEventListener("click", (event) => {
+    event.target.style.background = "#101820ff";
+    event.target.style.color = "#f2aa4cff";
+});
+
+const change3 = document.getElementById("image3");
+
+change3.addEventListener("click", (event) => {
+    event.target.style.background = "#ffd662ff";
+    event.target.style.color = "#00539cff";
+});
+
+const change4 = document.getElementById("image4");
+
+change4.addEventListener("click", (event) => {
+    event.target.style.background = "#606060ff";
+    event.target.style.color = "#d6ed17ff";
+});
+
+const change5 = document.getElementById("image5");
+
+change5.addEventListener("click", (event) => {
+    event.target.style.background = "#000";
+    event.target.style.color = "white";
+});
+
+const change6 = document.getElementById("image6");
+
+change6.addEventListener("click", (event) => {
+    event.target.style.background = "red";
+    event.target.style.color = "white";
+});
+
